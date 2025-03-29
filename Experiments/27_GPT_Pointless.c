54f496b2cd936832eff5f1806290f1a4e5f09a92
Murage Kibicho <murage.kibicho@gmail.com>
2025-03-29 16:31:18 +0300

    sdsd

---
+++ Experiments/27_GPT_Pointless.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+#include "cJSON.h" 
+
+int main()
+{
+	return 0;
+}
